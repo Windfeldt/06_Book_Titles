aff4f9ac764421172ccee80c37f07a97e59990ce
Windfeldt <Windfeldt@DESKTOP-C7LMTCM>
2016-09-19 09:56:10 +0200

    Add project files.

---
+++ 06_Book_Titles/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace _06_Book_Titles
+{
+    internal class Book
+    {
+        private string _title;
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+
+            set
+            {
+                
+                //_title = char.ToUpper(_title[0])+_title.Substring(1);
+                this._title = "Infernal";
+            }
+        }
+        
+    }
+}
